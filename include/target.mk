633763d7db1274537950f4c83e39f84636ff24d2
hungleung <hungleung.lee@me.com>
2019-01-10 11:41:32 +0800

    1st config change and lean package added
    add lean package
    change default package for x64

--- include/target.mk
+++ include/target.mk
@@ -13,11 +13,12 @@ __target_inc=1
 DEVICE_TYPE?=router
 
 # Default packages - the really basic set
-DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd
+DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd wget curl ipset-lists pdnsd-alt vlmcsd \
+luci-ssl-openssl luci-app-upnp luci-app-ddns luci-app-wol luci-app-ssr-pro luci-app-ssrserver-python luci-app-udpxy luci-app-vlmcsd
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
-DEFAULT_PACKAGES.router:=dnsmasq iptables ip6tables ppp ppp-mod-pppoe firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload
+DEFAULT_PACKAGES.router:=dnsmasq-full iptables ppp ppp-mod-pppoe firewall kmod-ipt-offload
 DEFAULT_PACKAGES.bootloader:=
 
 ifneq ($(DUMP),)
