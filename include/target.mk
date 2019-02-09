322f1df13dbb14d3f037b94ade06f331afb27772
hungleung <hungleung.lee@me.com>
2019-02-09 21:09:15 +0800

    udpxy cancelled
    udpxy cancelled
    nut & acme added

--- include/target.mk
+++ include/target.mk
@@ -13,8 +13,8 @@ __target_inc=1
 DEVICE_TYPE?=router
 
 # Default packages - the really basic set
-DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd wget curl bind-dig vlmcsd default-settings \
-luci-ssl-openssl luci-app-upnp luci-app-ddns luci-app-wol luci-app-ssrserver-python luci-app-udpxy luci-app-vlmcsd ipset-lists luci-app-ssr-pro
+DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd wget curl bind-dig vlmcsd default-settings nut acme acme-dnsapi \
+luci-ssl-openssl luci-app-upnp luci-app-ddns luci-app-wol luci-app-ssrserver-python luci-app-vlmcsd ipset-lists luci-app-ssr-pro LUCI_LANG_zh-cn
 # For nas targets
 # DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
